c7376b3ffb059cecf1f5a8c25b7641c88fc67626
takurooo <tn19880303@yahoo.co.jp>
2023-03-26 22:23:07 +0900

    improve readability

--- show_generator.py
+++ show_generator.py
@@ -24,6 +24,41 @@ def load_config(config_path):
     return j
 
 
+def create_datagen(config):
+    return ImageDataGenerator(
+        featurewise_center=config["featurewise_center"],
+        samplewise_center=config["samplewise_center"],
+        fill_mode=config["fill_mode"],
+        rotation_range=config["rotation_range"],
+        width_shift_range=config["width_shift_range"],
+        height_shift_range=config["height_shift_range"],
+        shear_range=config["shear_range"],
+        zoom_range=config["zoom_range"],
+        horizontal_flip=config["horizontal_flip"],
+        vertical_flip=config["vertical_flip"],
+        rescale=config["rescale"],
+    )
+
+
+def generate_images(datagen, img_path, num_images):
+    # open the image file as PIL format
+    img = image_utils.load_img(img_path)
+    # convert PIL format to ndarray format for datagen.flow
+    img_array = image_utils.img_to_array(img)
+    # (height, width, 3) -> (1, height, width, 3) for datagen.flow
+    x = img_array.reshape((1,) + img_array.shape)
+
+    imgs = []
+    for d in datagen.flow(x, batch_size=1):
+        # convert ndarray format to PIL format to display the image.
+        imgs.append(image_utils.array_to_img(d[0], scale=True))
+        # since datagen.flow loops infinitely, you need to break out of the loop
+        # once you have obtained the required number of images.
+        if (len(imgs) % num_images) == 0:
+            break
+    return imgs
+
+
 def show_imgs(imgs, row, col):
     """Show PIL format images as row*col
     # Arguments
@@ -44,40 +79,12 @@ def show_imgs(imgs, row, col):
 
 
 def main(args):
-    config = load_config(args.config_path)
     if not os.path.exists(args.img_path):
         raise ValueError(f"Invalid img_path: {args.img_path}")
 
-    # open the image file as PIL format
-    img = image_utils.load_img(args.img_path)
-    # convert PIL format to ndarray format for datagen.flow
-    x = image_utils.img_to_array(img)
-    # (height, width, 3) -> (1, height, width, 3) for datagen.flow
-    x = x.reshape((1,) + x.shape)
-
-    datagen = ImageDataGenerator(
-        featurewise_center=config["featurewise_center"],
-        samplewise_center=config["samplewise_center"],
-        fill_mode=config["fill_mode"],
-        rotation_range=config["rotation_range"],
-        width_shift_range=config["width_shift_range"],
-        height_shift_range=config["height_shift_range"],
-        shear_range=config["shear_range"],
-        zoom_range=config["zoom_range"],
-        horizontal_flip=config["horizontal_flip"],
-        vertical_flip=config["vertical_flip"],
-        rescale=config["rescale"],
-    )
-
-    imgs = []
-    for d in datagen.flow(x, batch_size=1):
-        # convert ndarray format to PIL format to display the image.
-        imgs.append(image_utils.array_to_img(d[0], scale=True))
-        # since datagen.flow loops infinitely, you need to break out of the loop
-        # once you have obtained the required number of images.
-        if (len(imgs) % IMAGE_NUM) == 0:
-            break
-
+    config = load_config(args.config_path)
+    datagen = create_datagen(config)
+    imgs = generate_images(datagen, args.img_path, IMAGE_NUM)
     show_imgs(imgs, ROW, COL)
 
 
