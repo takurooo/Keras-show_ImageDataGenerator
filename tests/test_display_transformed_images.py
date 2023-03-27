7e7b269484bd7834ed08c28b1636e593cbb57144
takurooo <tn19880303@yahoo.co.jp>
2023-03-28 00:55:10 +0900

    Add unit test for         generate_transformed_images(datagen, invalid_img_path, num_imgs)

--- tests/test_display_transformed_images.py
+++ tests/test_display_transformed_images.py
@@ -1,9 +1,22 @@
 import json
 
 import pytest
+from PIL import Image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-from display_transformed_images import create_image_data_generator, load_json
+from display_transformed_images import (
+    create_image_data_generator,
+    generate_transformed_images,
+    load_json,
+)
+
+
+@pytest.fixture
+def tmp_test_image(tmp_path):
+    img = Image.new("RGB", (50, 50), color=(73, 94, 107))
+    img_path = tmp_path / "test_image.jpg"
+    img.save(img_path)
+    return img_path
 
 
 def test_load_json_valid_file(tmp_path):
@@ -55,3 +68,26 @@ def test_create_image_data_generator_invalid_config():
     invalid_config = {"rotation_range": 0, "invalid_key": 0}
     with pytest.raises(ValueError, match=r"Invalid config key or value in .*"):
         create_image_data_generator(invalid_config)
+
+
+def test_generate_transformed_images_valid_args(tmp_test_image):
+    datagen = ImageDataGenerator(rotation_range=40)
+    num_imgs = 5
+    imgs = generate_transformed_images(datagen, tmp_test_image, num_imgs)
+    assert len(imgs) == num_imgs
+    assert all(isinstance(img, Image.Image) for img in imgs)
+
+
+def test_generate_transformed_images_invalid_img_path():
+    datagen = ImageDataGenerator(rotation_range=40)
+    invalid_img_path = "non_existent_image.jpg"
+    num_imgs = 5
+    with pytest.raises(ValueError, match=r"Invalid image_path: .* doesn't exist"):
+        generate_transformed_images(datagen, invalid_img_path, num_imgs)
+
+
+def test_generate_transformed_images_invalid_num_imgs(tmp_test_image):
+    datagen = ImageDataGenerator(rotation_range=40)
+    invalid_num_imgs = 0
+    with pytest.raises(ValueError, match=r"num_imgs must be a positive integer\."):
+        generate_transformed_images(datagen, tmp_test_image, invalid_num_imgs)
